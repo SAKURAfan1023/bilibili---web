deb61697e20675c22af38e2ee903e0a93c6dac54
SAKURA <1023lifanhu@gmail.com>
2025-04-21 20:09:31 +0800

    完善了登录页面的密码登录

--- Front-end/src/common/utils/pc/pc-common/pc-loginPage.js
+++ Front-end/src/common/utils/pc/pc-common/pc-loginPage.js
@@ -1,4 +1,10 @@
 const closeButton = document.querySelector('.close-pc-login')
+const closeEye = document.querySelector('.pc-login-right-inputTop-closeEye')
+const openEye = document.querySelector('.pc-login-right-inputTop-openEye')
+const password = document.querySelector('.pc-login-right-inputTop-password')
+const userName = document.querySelector('.pc-login-right-inputTop-userName')
+const login = document.querySelector('.pc-login-right-inputButton-right')
+const form = document.querySelector('.pc-login-form-password')
 
 function hideTheLoginPage() {
   requestAnimationFrame(() => { document.querySelector('.pc-loginMask').style.display = 'none' })
@@ -8,7 +14,35 @@ function openTheLoginPage() {
   document.querySelector('.pc-loginMask').style.display = 'block'
 }
 
+function passwordChange() {
+  password.addEventListener('input', () => {
+    password.type = 'password'
+  })
 
+  closeEye.addEventListener('click', () => {
+    password.type = 'text'
+    closeEye.style.display = 'none'
+    openEye.style.display = 'inline-block'
+  })
+
+  openEye.addEventListener('click', () => {
+    password.type = 'password'
+    closeEye.style.display = 'inline-block'
+    openEye.style.display = 'none'
+  })
+}
+
+
+login.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(serialize(form, { hash: true, empty: true }));
+})
+
+
+
+
+
+passwordChange()
 document.querySelectorAll('.pc-index-header-imgBorder').forEach(item => { item.addEventListener('click', openTheLoginPage) })
 document.querySelectorAll('.pc-loginCard-content4-button').forEach(item => { item.addEventListener('click', openTheLoginPage) })
 document.querySelectorAll('.pc-loginCard-content3').forEach(item => { item.addEventListener('click', openTheLoginPage) })
