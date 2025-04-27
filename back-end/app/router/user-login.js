a3766fdf2e11a5c19018375b1e83abe31ed5ae81
SAKURA <1023lifanhu@gmail.com>
2025-04-27 20:38:10 +0800

    完成了用户注册和登录对密码的加密

--- back-end/app/router/user-login.js
+++ back-end/app/router/user-login.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 
 const secretKey = 'HuLiFan (>w<)'
 const router = express.Router()
 
+//注册
 const sendUserInfo = router.post('/user/signup', (req, res) => {
   const db = req.app.locals.db
   const postStr = 'insert into user set ?'
   const body = req.body
+  body.passWord = bcrypt.hashSync(body.passWord, 5)
   let message
   let status = 1
   db.query(postStr, body, (err, result) => {
@@ -41,14 +44,14 @@ const sendUserInfo = router.post('/user/signup', (req, res) => {
 })
 
 
-
+//登录
 const getUserInfo = router.post('/user/signin', (req, res) => {
   const db = req.app.locals.db
   const body = req.body
   let status = 0
   let message
-  const signinStr = 'select * from user where username = ? and password = ?'
-  db.query(signinStr, [body.userName, body.passWord], (err, result) => {
+  const signinStr = 'select * from user where username = ?'
+  db.query(signinStr, body.userName, (err, result) => {
     if (err) {
       status = 1
       message = '请求发送成功，数据库报错'
@@ -60,16 +63,29 @@ const getUserInfo = router.post('/user/signin', (req, res) => {
       })
     } else {
       console.log(result);
+      console.log(body.passWord);
+
       if (result.length != 0) {
-        message = '账号检测成功，验证通过'
-        status = 0
-        const token = jwt.sign({ username: body.userName }, secretKey, { expiresIn: '24h' })
-        return res.send({
-          status,
-          message,
-          data: body,
-          token
-        })
+        // body.passWord = bcrypt.hashSync(body.passWord,5)
+        if (bcrypt.compareSync(body.passWord, result[0].password)) {
+          message = '账号检测成功，验证通过'
+          status = 0
+          const token = jwt.sign({ username: body.userName }, secretKey, { expiresIn: '24h' })
+          return res.send({
+            status,
+            message,
+            data: body,
+            token
+          })
+        } else {
+          status = 1
+          message = '密码输入错误，请重新输入'
+          res.send({
+            status,
+            message,
+            data: body
+          })
+        }
       } else {
         message = '用户名或密码错误，请重新输入'
         status = 1
@@ -83,7 +99,7 @@ const getUserInfo = router.post('/user/signin', (req, res) => {
   })
 })
 
-
+//登录验证
 const verifyToken = router.post('/user/verify', (req, res) => {
   let message = '自动登录成功'
   res.send({
