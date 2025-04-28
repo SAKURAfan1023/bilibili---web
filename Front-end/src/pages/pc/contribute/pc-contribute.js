2c813c789e2822603e636859445efc1862570cc7
SAKURA <1023lifanhu@gmail.com>
2025-04-28 20:16:53 +0800

    修复了存在的bug，为生产环境适配

---
+++ Front-end/src/pages/pc/contribute/pc-contribute.js
@@ -0,0 +1,11 @@
+import '@/common/utils/pc/pc-common/pc-loginPop.js'
+import '@/common/utils/pc/pc-common/pc-loginPage.js'
+import '@/common/utils/pc/pc-common/pc-loginUpload.js'
+import '@/common/utils/pc/pc-common/pc-loginAuto.js'
+import '@/common/utils/pc/index/pc-index-fixedHeader.js'
+import '@/common/utils/pc/index/pc-index-iconJump.js'
+
+
+
+
+import '../index/pc-index-header.less'
