a368789daf67e99b53d6df9fa3d660c5d822e478
SAKURA <1023lifanhu@gmail.com>
2025-04-30 23:17:40 +0800

    开始alert模块

--- Front-end/src/common/utils/pc/pc-common/pc-alert.js
+++ Front-end/src/common/utils/pc/pc-common/pc-alert.js
@@ -1,5 +1,8 @@
 
 
-function alertMsg() {
-
+export function alertMsg(message) {
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <div style="width: 80%; fontSize: 18px; lineHight:30px; display: position; left: 0; top: 0; backgroundColor: pink">${message}</div>
+  `
 }
